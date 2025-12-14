8e0fe4ba3106ffc97cbefeefdcb7acd927bc8f10
NandhiniNandagopal <nandhininandagopal2007@gmail.com>
2025-12-14 13:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open('/content/drive/MyDrive/INTERNSHIP/NANDHINI/insurance_model.pkl', 'rb') as f:
+with open('insurance_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 st.title("Insurance Premium Predictor")
@@ -17,3 +17,4 @@ if st.button("Predict"):
     input_data = np.array([[age]])
     premium = model.predict(input_data)
     st.success(f"Estimated Insurance Premium: ₹{premium[0]:.2f}")
+
